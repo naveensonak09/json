754631ecc3ea85730db956a65b1c919225a2fdce
naveen kumar <naveen_kumar@excellencetechnologies.in>
2020-07-29 18:07:39 +0530

    creating json object

---
+++ js/object.js
@@ -0,0 +1,4 @@
+var JSONObj = { "firstname" : "naveen", "lastname":"kumar" ,"age"  : 25 };
+document.write("Name = "+JSONObj.firstname+"<br>");  
+document.write("Last Name = "+JSONObj.lastname+"<br>");  
+document.write("Year = "+JSONObj.age);  
